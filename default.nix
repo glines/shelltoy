e8457acd362940a5b817d58e1d6ce7840cc890cb
Jonathan Glines <jonathan@glines.net>
2016-11-19 15:53:42 -0700

    nix package building for version 0.0.1

--- default.nix
+++ default.nix
@@ -1,8 +1,16 @@
-{ stdenv, cmake, libtsm, SDL2, glew, freetype, dejavu_fonts, pkgconfig, check }:
+{ stdenv, fetchgit, cmake, libtsm, SDL2, glew, freetype, dejavu_fonts, pkgconfig, check, vimNox }:
 
 stdenv.mkDerivation rec {
   name = "shelltoy-${version}";
   version = "0.0.1";
 
-  buildInputs = [ cmake libtsm SDL2 glew freetype dejavu_fonts pkgconfig check ];
+  src = fetchgit {
+    url = "file:///home/auntieneo/code/shelltoy";
+    rev = "8c3e5938d91a1251adc8cd4e97b4507bff14f850";
+    sha256 = "0mnc0pv9cavcz1lg4cwrhj7hslss47w05z2lhqwp5x5jkgsq9c8c";
+  };
+
+  buildInputs = [ cmake libtsm SDL2 glew freetype dejavu_fonts pkgconfig check
+    vimNox  /* For the xxd utility */
+  ];
 }
