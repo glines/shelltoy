ae9e8017663e6f498513fba5510e39373748eceb
Jonathan Glines <jonathan@glines.net>
2016-11-23 12:19:24 -0700

    Updated nix package to 0.0.2.

--- default.nix
+++ default.nix
@@ -2,16 +2,15 @@
 
 stdenv.mkDerivation rec {
   name = "shelltoy-${version}";
-  version = "0.0.1";
+  version = "0.0.2";
 
   src = fetchgit {
     url = "file:///home/auntieneo/code/shelltoy";
-    rev = "8c3e5938d91a1251adc8cd4e97b4507bff14f850";
-    sha256 = "0mnc0pv9cavcz1lg4cwrhj7hslss47w05z2lhqwp5x5jkgsq9c8c";
+    rev = "f87a70404dccefbe94ba2a72dc2860c6f86f9df2";
+    sha256 = "1w3jlpq12v3vqha2xg9mrjfgx372k2bfna5c1ih6rfsndr83fpia";
   };
 
   buildInputs = [ cmake libtsm SDL2 glew freetype dejavu_fonts pkgconfig check
     vimNox  /* For the xxd utility */
-    gprof2dot oprofile  /* For profiling */
   ];
 }
