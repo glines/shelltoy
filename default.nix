56bcb29e3ab3804bd7f325aa43214be0e2b67ba0
Jonathan Glines <jonathan@glines.net>
2017-02-07 00:38:54 -0700

    Nix build working for 0.0.5

--- default.nix
+++ default.nix
@@ -1,18 +1,21 @@
-{ stdenv, fetchgit, cmake, libtsm, SDL2, glew, freetype, dejavu_fonts, pkgconfig, check, vimNox, gprof2dot, oprofile, jansson, fontconfig, expat, doxygen, python27Packages }:
+{ stdenv, fetchgit, cmake, libtsm, SDL2, glew, freetype, dejavu_fonts, pkgconfig, check, vimNox, gprof2dot, oprofile, jansson, fontconfig, expat, doxygen, python27Packages, git }:
 
 stdenv.mkDerivation rec {
   name = "shelltoy-${version}";
-  version = "git";
+  version = "0.0.5";
 
   src = fetchgit {
     url = "file:///home/auntieneo/code/shelltoy";
-    rev = "8b90674f634b7304ce4f75bd24c9beafa6564479";
-    sha256 = "1avy1gp900ld1y882pij3v3bhi0wpxz9fjjivivpmzvikxymhr6w";
+    rev = "refs/tags/${version}";
+    sha256 = "13l2jy07ykj0zk6bq8zss8j55ly4wkhffl8p3rddbb3r0bsr63nd";
+    leaveDotGit = true;  /* Needed for version tags */
+    deepClone = true;
   };
 
   buildInputs = [ cmake libtsm SDL2 glew freetype dejavu_fonts pkgconfig check
     jansson fontconfig expat doxygen
     vimNox  /* For the xxd utility */
     python27Packages.sphinx
+    git
   ];
 }
